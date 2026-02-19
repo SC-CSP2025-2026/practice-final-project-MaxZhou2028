e6ef975b0744c14e2a112228a9ecd40654f4784b
Max Zhou <Zixing.Zhou@scsstudent.org>
2026-02-19 14:48:46 -0800

    feat : Add search functionality

--- website/js/script.js
+++ website/js/script.js
@@ -1,35 +1,25 @@
-const searchBtn = document.getElementById("searchBtn");
-const movieInput = document.getElementById("movieInput");
-const resultsList = document.getElementById("resultsList");
+const generalButton = document.getElementById("searchBtn");
 
 async function searchMovie(name) {
-
   const url = `https://student-api-proxy.onrender.com/api/advanced-movie-search.p.rapidapi.com/search/movie?query=${name}`;
 
   const options = {
     method: "GET",
     headers: {
-      "X-API-Key": "2c72fe41164758c4f9e06e6d3d06fbab59b0088e893afb8cd56dafc34a03b6ee",
+      "X-API-Key":
+        "2c72fe41164758c4f9e06e6d3d06fbab59b0088e893afb8cd56dafc34a03b6ee",
     },
   };
 
-
   const response = await fetch(url, options);
-  const data = await response.json();
-
-  console.log(data)
-
+  const result = await response.json();
+  const data = getsamplemoviesresult.data; //const data = getsamplemovies();
+  const movie = data.body;
 }
 
-searchMovie("Avengers");
-
-searchBtn.addEventListener("click", () => {
-  const movieName = movieInput.value.trim();
-  
-  searchMovie(movieName);
-  
+jokes.forEach((movie) => {
+  console.log(movie.setup);
+  console.log(movie.punchline);
 });
 
-
-
-
+loadjokesByType("knock-knock");
